de49b9cca2a8cab245ca8c4f24066cb21f8454dc
Ganesh Panja <ganesh@Ganeshs-MacBook-Pro.local>
2026-02-16 11:26:03 -0500

    samplet test

---
+++ practice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sample{
+    public static void main(String[] args) {
+        int a = 10;
+        int b=30;
+
+        int c = a+b;
+        System.out.println(c);
+
+        if(c>20){
+
+            System.out.println("c is greater than 20");
+        }
+        else{
+            System.out.println("c is less than 20");
+        }  
+        
+        
+}
